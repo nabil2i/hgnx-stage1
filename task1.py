485049440eecaa2b7102c441e7425d6cac0b1c72
Nabil <nabil.noble01@gmail.com>
2023-09-07 15:18:13 +0000

    add requirements

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """backend endpoint"""
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask import Flask, jsonify, request
 
@@ -16,19 +16,34 @@ def hgnx_info():
   if not slack_name or not track:
     return jsonify({'error': 'slack_name and track are required'}), 400
   
-  current_time = datetime.utcnow();
+  current_utc_time = datetime.utcnow()
+  tolerance = timedelta(minutes=2)
   
+  lower_bound = current_utc_time - tolerance
+  upper_bound = current_utc_time + tolerance
+  
+  if not (lower_bound <= current_utc_time <= upper_bound):
+    return jsonify({'error': 'Current UTC time is not valid'}), 400
+  
+  current_day = current_utc_time.strftime("%A")
+  utc_time = current_utc_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+  github_repo_url = "https://github.com/nabil2i/hgnx-stage1"
+  github_file_url = "https://github.com/nabil2i/hgnx-stage1/blob/main/task1.py"
+  status_code = 200
+
   response_data = {
     "slack_name": slack_name,
-    "current_day": current_time.strftime("%A"),
-    "utc_time": current_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
+    "current_day": current_day,
+    "utc_time": utc_time,
     "track": track,
-    "github_file_url": "https://github.com/nabil2i/hgnx-stage1/blob/main/task1.py",
-    "github_repo_url": "https://github.com/nabil2i/hgnx-stage1",
-    "status_code": 200
+    "github_file_url": github_file_url,
+    "github_repo_url": github_repo_url,
+    "status_code": status_code
   }
   
   return jsonify(response_data), 200
 
 if __name__ == '__main__':
-  app.run()
+  host = '0.0.0.0'
+  port = 5000
+  app.run(host, port)
